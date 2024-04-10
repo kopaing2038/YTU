e4fb879b75cf04c7f014012fc3df785aefa5acf4
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-04-10 22:49:32 +0630

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -6,31 +6,6 @@ from ..config import Config
 from ..utubebot import UtubeBot
 
 
-class ChatAction:
-    def __init__(self, action):
-        self.action = action
-        self.value = action  # Assigning the action as the value
-
-    def __str__(self):
-        return self.action
-
-    def __eq__(self, other):
-        return isinstance(other, ChatAction) and self.action == other.action
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self):
-        return hash(self.action)
-
-    def to_dict(self):
-        return {"action": self.action}
-
-    @staticmethod
-    def from_dict(data):
-        return ChatAction(data["action"])
-
-
 @UtubeBot.on_message(
     Filters.private
     & Filters.incoming
@@ -38,10 +13,9 @@ class ChatAction:
     & Filters.user(Config.AUTH_USERS)
 )
 async def _start(c: UtubeBot, m: Message):
-    await c.send_chat_action(m.chat.id, action=ChatAction("typing"))
+    await m.reply_chat_action("typing")
     await m.reply_text(
         text=tr.START_MSG.format(m.from_user.first_name),
         quote=True,
         disable_web_page_preview=True,
-    )
-
+            )
