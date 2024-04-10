1c53612f94a863a314772e42a8417f602b17785e
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-04-10 21:37:24 +0630

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -7,7 +7,7 @@ class UtubeBot(Client):
     def __init__(self):
         super().__init__(
             
-            name= "str",
+            name= Config.SESSION_NAME,
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
