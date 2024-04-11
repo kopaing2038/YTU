adfadeb7890597d1112c93e4d238ba9afc7dbaa1
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-04-11 13:42:00 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,29 @@
 import os
 import time
 import random
-from pyrogram import Client, filters
 from pytube import YouTube
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from google.oauth2 import service_account
+from pyrogram import Client, filters
 
 
 YOUTUBE_API_KEY = 'AIzaSyAqY869WGpWfSBKGdvLJWlbd8YkreNym30'
 YOUTUBE_CHANNEL_ID = 'UCy6J6n_IfRethp32skMk5rQ'
 
 
-from pyrogram import Client, filters
-from pyrogram.types import Message
-import os
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-
 # Telegram API credentials
 api_id = "21970746"
 api_hash = "32deb816dc3874e871b6158673fd3683"
 bot_token = "7191544925:AAF1wNdb4SfdbzM6-691e0eNio4EmAqkRQ4"
+
+
+# Google OAuth 2.0 credentials
+google_client_id = "298717439916-cek7ibrb563mju3u0p5c068q4bp4d34k.apps.googleusercontent.com"
+google_client_secret = "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"
+redirect_uri = "https://youtube.com"  # Replace with your actual redirect URI
 
 # Google API credentials
 credentials = service_account.Credentials.from_service_account_file('credentials.json')
