4daeedde12c5c0f988f7303a8417b55b2e162c08
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-04-11 13:52:25 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Authenticate YouTube API
 flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-credentials = flow.run_local_server()
+credentials = flow.run_console()
 
 youtube = build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
